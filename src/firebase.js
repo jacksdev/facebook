462069246c1acd3ccce5843f3a5e9ee73fb7c88b
abc <jacksdevwork@gmail.com>
2021-08-03 15:16:01 -0700

    firebaes set up

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCTmVI6EXC3WGWjXhnhcBlMdKnoMMBngpg",
+	authDomain: "facebook-e5fbc.firebaseapp.com",
+	projectId: "facebook-e5fbc",
+	storageBucket: "facebook-e5fbc.appspot.com",
+	messagingSenderId: "711029701774",
+	appId: "1:711029701774:web:b560bc60d225acd5d65ddf"
+      };
