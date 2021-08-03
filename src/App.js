dd0ada1a071119e6fae58524fa64851652be8ed4
abc <jacksdevwork@gmail.com>
2021-08-03 16:25:00 -0700

    set up header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 
+import styled from 'styled-components';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Facebook</h1>
+      <BodyContainer>
+        <Header />
+      </BodyContainer>
+      
     </div>
   );
 }
 
 export default App;
+
+
+const BodyContainer = styled.div`
+  background-color: #d5d6e2;
+	display: flex;
+  height: 100vh;
+  justify-content: center;
+`
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import styled from 'styled-components';
+import logo from './logo_fb.png'
 
 function Header() {
 		return (
-			<div>
-				
-			</div>
+			<HeaderContainer>
+				<img src={logo} />
+			</HeaderContainer>
 		)
 }
 
 export default Header
+
+
+const HeaderContainer = styled.div`
+	height: 65px;
+	border-radius: 10px 10px 0 0;
+	margin-top: 50px;
+	background-color: #506ac5;
+	width: 80%;
+`
